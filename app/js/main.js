305b56c8b99d8012ca21bcac7d248d7ef05c83fe
maxclasher1 <madaminov.aaaaa@gmail.com>
2021-02-15 23:37:00 +0500

    added new-products and video sections

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,17 @@ $(function() {
     autoplay: true
   });
   
-  var mixer = mixitup('.products__items');
+
+  var mixer1 = mixitup('.products__items', {
+    selectors: {
+      control: '.filter-1'
+    }
+  });
+
+  var mixer2 = mixitup('.new-products__items', {
+    selectors: {
+      control: '.filter-2'
+    }
+  });
+
 });
